4337f298b58c3a8dc170e3b812cef75ddaee3e1a
Christoffer Klang <christoffer.klang@gmail.com>
2012-07-29 01:15:19 +0200

    Add ReadmillClient#buildRequest

--- spec/ReadmillClient_spec.js
+++ spec/ReadmillClient_spec.js
@@ -4,12 +4,59 @@ var helpers = require('./spec_helpers');
 
 describe('ReadmillClient', function() {
 
-  beforeEach(function() {
-    helpers.mockRequests();
-  });
+  describe('buildRequest', function() {
 
-  it('can be instantiated', function() {
-    expect(function() { new ReadmillClient() }).not.toThrow();
+    it('allows case by case configuration', function() {
+      var client = new ReadmillClient();
+      var requestOptions = client.buildRequest({
+        host: 'test.host.com',
+        port: 9001,
+        method: 'test'
+      });
+
+      expect(requestOptions.method).toEqual('test');
+      expect(requestOptions.host).toEqual('test.host.com');
+      expect(requestOptions.port).toEqual(9001);
+    });
+
+    it('allows client based configuration', function() {
+      var client = new ReadmillClient({
+        host: 'client.host.com',
+        port: 8001,
+        method: 'client-test'
+      });
+
+      var requestOptions = client.buildRequest();
+
+      expect(requestOptions.method).toEqual('client-test');
+      expect(requestOptions.host).toEqual('client.host.com');
+      expect(requestOptions.port).toEqual(8001);
+    });
+
+    it('falls back to defaults', function() {
+      var client = new ReadmillClient();
+      var requestOptions = client.buildRequest();
+
+      expect(requestOptions.method).toEqual('get');
+      expect(requestOptions.host).toEqual('api.readmill.com');
+      expect(requestOptions.port).toEqual(80);
+    });
+
+    it('authenticates requests with client id', function() {
+      var client = new ReadmillClient({ clientId: 'my-client-id' });
+      var requestOptions = client.buildRequest();
+      expect(requestOptions.headers['Authorization']).toEqual('ClientId my-client-id');
+    });
+
+    it('authenticates requests with access token', function() {
+      var client = new ReadmillClient({
+        accessToken: 'haxortoken',
+        clientId: 'my-client-id'
+      });
+      var requestOptions = client.buildRequest();
+      expect(requestOptions.headers['Authorization']).toEqual('OAuth haxortoken');
+    });
+
   });
 
 });
